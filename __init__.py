d245e668a0cd8ce07e9480341c9bcd8b706448ef
Geert Hesselink <geert.hess@gmail.com>
2025-07-27 13:00:33 +0100

    Use _ValidationError only internally

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 try:
     from parser.parse import parse
     from parser.file import file, open
-    from parser.errors import ValidationError, CollectedValidationErrors, DuplicateNameError, HeaderFieldError
+    from parser.errors import _ValidationError, CollectedValidationErrors, DuplicateNameError, HeaderFieldError
 except:
     from .parser.parse import parse 
     from .parser.file import file, open 
-    from .parser.errors import ValidationError, CollectedValidationErrors, DuplicateNameError, HeaderFieldError
+    from .parser.errors import _ValidationError, CollectedValidationErrors, DuplicateNameError, HeaderFieldError
 
-__all__ = ["parse", "open", "file", "ValidationError", 
+__all__ = ["parse", "open", "file", "_ValidationError", 
            "CollectedValidationErrors", "DuplicateNameError", "HeaderFieldError"] # for testing 
